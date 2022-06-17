7f5f1c4b2e73a7d68343036d254f420414b9f630
Krishi Jain <krishijain@fb.com>
2022-06-17 15:37:45 -0700

    UI Alert

--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -38,7 +38,12 @@
     self.filteredData = self.movies;
     
     // UI alert
+    UIAlertController *networkAlert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Agin" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action){
+        [self fetchMovies];
+    }];
     
+    [networkAlert addAction:tryAgainAction];
     
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -46,6 +51,11 @@
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
                NSLog(@"%@", [error localizedDescription]);
+               
+               //Alert UI
+               [self presentViewController:networkAlert animated:YES completion:^{
+                   //Optional code for after alert controller has finished presenting
+               }];
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
