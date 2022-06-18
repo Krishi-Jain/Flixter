7521fd1b76b50efe3a74db4e8f1aef9d24779144
Krishi Jain <krishijain@fb.com>
2022-06-18 11:45:02 -0700

    Grid View Display

--- Flixter/GridViewController.m
+++ Flixter/GridViewController.m
@@ -6,19 +6,70 @@
 //
 
 #import "GridViewController.h"
+#import "GridCell.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface GridViewController ()
+@interface GridViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
 
 
 @end
 
 @implementation GridViewController
 
+static NSString * const reuseIdentifier = @"Cell";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    //self.collectionView.dataSource = self;
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    [self.fetchMovies];
+    [self.collectionView registerClass:[UICollectionViewCell] forCellWithReuseIdentifier:reuseIdentifier];
 }
+
+- (void)fetchMovies{
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+               self.movies = dataDictionary[@"results"];
+               NSLog(@"%@", self.movies);
+               for (NSDictionary *movie in self.movies){
+                   NSLog(@"%@", movie[@"title"]);
+               }
+           }
+        [self.collectionView reloadData];
+       }];
+    [task resume];
+}
+
+#pragma mark <UICollectionViewDataSource>
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return self.movies.count;
+}
+
+-(__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CollectionCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"CollectionCell" forIndexPath:indexPath];
+    NSDictionary *movie = self.movies[indexPath.row];
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.cellView.image = nil;
+    [cell.cellView setImageWithURL:posterURL];
+    return cell;
+}
+#pragma mark <UICollectionViewDelegate>
 
 /*
 #pragma mark - Navigation
