c6911092f36766b0ccb3d813780ea7992ae6d00f
Krishi Jain <krishijain@fb.com>
2022-06-17 19:36:19 -0700

    Collection View in progress

---
+++ Flixter/GridCell.h
@@ -0,0 +1,17 @@
+//
+//  GridCell.h
+//  Flixter
+//
+//  Created by Krishi Jain on 6/17/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GridCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flixter/GridViewController.m
@@ -0,0 +1,33 @@
+//
+//  GridViewController.m
+//  Flixter
+//
+//  Created by Krishi Jain on 6/17/22.
+//
+
+#import "GridViewController.h"
+
+@interface GridViewController ()
+
+
+@end
+
+@implementation GridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    //self.collectionView.dataSource = self;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
